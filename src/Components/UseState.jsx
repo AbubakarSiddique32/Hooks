772c2016d5d395e4d42be4d642428949026f22aa
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-06-11 17:30:28 +0500

    Hooks

--- src/Components/UseState.jsx
+++ src/Components/UseState.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UseState = () => {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div className=" bg-slate-400 flex flex-col justify-center items-center text-center p-10">
+      <h1 className=" text-blue-950 font-bold text-[40px] mb-5">
+        Hi this is UseState Hook
+      </h1>
+
+      <button
+        className="border mb-5 border-white p-2 px-5 rounded-full text-black font-bold hover:shadow-2xl hover:bg-slate-200 hover:scale-105"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        Click Me
+      </button>
+      <p className=" bg-slate-100 text-blue-900 p-5 rounded-full">
+        This Is Counter {count}
+      </p>
+    </div>
+  );
 };
 
 export default UseState;
