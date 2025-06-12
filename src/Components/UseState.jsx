cd94977ffeae918867734a968756fe64f7c3f81a
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-06-12 17:59:33 +0500

    Hooks

--- src/Components/UseState.jsx
+++ src/Components/UseState.jsx
@@ -2,41 +2,56 @@ import React, { useState } from "react";
 
 const UseState = () => {
   const [count, setCount] = useState(0);
+  const [isVisible, setIsVisible] = useState();
   return (
-    <div className=" bg-slate-400 flex flex-col justify-center items-center text-center p-10">
-      <h1 className=" text-blue-950 font-bold text-[40px] mb-5">
-        Hi this is UseState Hook
-      </h1>
+    <div className=" w-full">
+      {/* 1st Example */}
+      <div className=" bg-slate-400 flex w-[90%] m-auto rounded-[20px] mt-5 flex-col justify-center items-center text-center p-10">
+        <h1 className=" text-blue-950 font-bold text-[40px] mb-5">
+          Hi this is UseState Hook
+        </h1>
 
-      <button
-        className="border mb-5 border-white p-2 px-5 rounded-full text-black font-bold hover:shadow-2xl hover:bg-slate-200 hover:scale-105"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        Increase
-      </button>
-      <p className=" bg-slate-100 text-blue-900 p-5 rounded-full">
-        This Is Counter {count}
-      </p>
-      {/* 2ND */}
-      <button
-        className="border mb-5 border-white p-2 px-5 rounded-full text-black font-bold hover:shadow-2xl hover:bg-slate-200 hover:scale-105"
-        onClick={() => setCount((count) => count - 1)}
-      >
-        Decrease
-      </button>
-      <p className=" bg-slate-100 text-blue-900 p-5 rounded-full">
-        This Is Counter {count}
-      </p>
-      {/* 3rd */}
-      <button
-        className="border mb-5 border-white p-2 px-5 rounded-full text-black font-bold hover:shadow-2xl hover:bg-slate-200 hover:scale-105"
-        onClick={() => setCount(0)}
-      >
-        Reset
-      </button>
-      <p className=" bg-slate-100 text-blue-900 p-5 rounded-full">
-        This Is Counter {count}
-      </p>
+        <p className=" bg-slate-100 text-blue-900 p-5 rounded-full">
+          This Is Counter {count}
+        </p>
+        <div className=" mt-5 flex gap-5">
+          <button
+            className="border mb-5 border-white p-2 px-5 rounded-full text-black font-bold hover:shadow-2xl hover:bg-slate-200 hover:scale-105"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            Increase
+          </button>
+
+          {/* 2ND */}
+          <button
+            className="border mb-5 border-white p-2 px-5 rounded-full text-black font-bold hover:shadow-2xl hover:bg-slate-200 hover:scale-105"
+            onClick={() => setCount((count) => count - 1)}
+          >
+            Decrease
+          </button>
+
+          {/* 3rd */}
+          <button
+            className="border mb-5 border-white p-2 px-5 rounded-full text-black font-bold hover:shadow-2xl hover:bg-slate-200 hover:scale-105"
+            onClick={() => setCount(0)}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+
+      {/* 2nd Example */}
+      <div className=" bg-green-500 flex w-[90%] m-auto rounded-full mt-10 p-5 justify-center items-center flex-col">
+        <button
+          className=" bg-green-950 text-white py-3 px-6 rounded-lg"
+          onClick={() => setIsVisible(!isVisible)}
+        >
+          {isVisible ? "Hide" : "Show"} Text
+        </button>
+        {isVisible && (
+          <p className=" w-full text-center mt-5">This Text is Visible!</p>
+        )}
+      </div>
     </div>
   );
 };
